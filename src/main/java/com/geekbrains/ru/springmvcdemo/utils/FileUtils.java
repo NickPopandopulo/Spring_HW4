087dae73e2322a6d74c0710bd2eea0d4b100cd29
Nikita Popandopulo <nikas.popandopulos@gmail.com>
2021-10-19 15:43:55 +0700

    Homework 4

--- src/main/java/com/geekbrains/ru/springmvcdemo/utils/FileUtils.java
+++ src/main/java/com/geekbrains/ru/springmvcdemo/utils/FileUtils.java
@@ -9,14 +9,13 @@ import java.nio.file.Paths;
 public class FileUtils {
 
     public static Path saveProductImage(MultipartFile file) {
-//        Path filepath = Path.of(Paths.get(System.getProperty("user.dir"), "data").toFile().getAbsolutePath());
-//        try {
-//            file.transferTo(filepath);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return filepath;
-        return  null;
+        Path filepath = Path.of(Paths.get(System.getProperty("user.dir"), "data").toFile().getAbsolutePath());
+        try {
+            file.transferTo(filepath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return filepath;
     }
 
 }
